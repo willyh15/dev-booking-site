bfa48346cb239d9fc8625d6cca935f8c3574cdef
Will Harrison <willharrison2016@gmail.com>
2024-10-22 11:52:28 -0500

    Update urls.py

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('appointments.urls')),
+    path('', include('appointments.urls')),  # Include the URLs for the appointments app
 ]
